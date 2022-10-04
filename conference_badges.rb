0367fbfab483650faef26f588d7e29c71779523d
Bobby Hinton <bob.g.hint@gmail.com>
2022-10-03 21:14:48 -0500

    commit tree 6bf877126c6924765c4dea91f894117c2e28a43b

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+
+def badge_maker name
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator names
+  names.map{|name| badge_maker(name)}
+end
+
+# use the each_with_index to assign a room to each person
+# then 
+
+def assign_rooms names
+ names.map.with_index {|name,index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+end
+
+def printer(names)
+  batch_badge_creator(names).each do |value|
+    puts value
+  end
+  assign_rooms(names).each do |value|
+    puts value
+  end
+end
